dae8e4107961eef25f69a79a7897d9e086e75584
Arda Torun <ardamerttorun@gmail.com>
2023-05-27 21:28:05 +0300

    Constructors

--- musicplayer.js
+++ musicplayer.js
@@ -3,4 +3,24 @@ class MusicPlayer{
         this.musicList=musicList;
         this.index=0;
     }
+
+    getMusic(){
+        return this.musicList[this.index];
+    }
+
+    next(){
+        if(this.index+1 !=this.musicList.length){
+            this.index++;
+        }else{
+            this.index=0;
+        }
+    }
+
+    previous(){
+        if(this.index != 0){
+            this.index--;
+        }else{
+            this.index=this.musicList-1;
+        }
+    }
 }
