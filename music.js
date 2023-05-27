06a7a2595128ec0a1fb9bcdf5dc64dc345486f78
Arda Torun <ardamerttorun@gmail.com>
2023-05-27 21:49:40 +0300

    Display Music

--- music.js
+++ music.js
@@ -13,7 +13,7 @@ class Music{
 
 
 const musicList=[
-    new Music("Boşver","Nilüfer","1.jpg","1.mp3"),
-    new Music("Bu da Geçer mi","Yalın","2.jpg","2.mp3"),
-    new Music("Aramızda Uçurumlar","Suat Suna","3.jpg","3.mp3")
+    new Music("Boşver","Nilüfer","1.jpeg","1.mp3"),
+    new Music("Bu da Geçer mi","Yalın","2.jpeg","2.mp3"),
+    new Music("Aramızda Uçurumlar","Suat Suna","3.jpeg","3.mp3")
 ]
